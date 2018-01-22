40ccb2363953816bd071efa7dee0649571cf535c
rosemer <35221895+rosemer@users.noreply.github.com>
2018-01-22 16:20:55 +0530

    digit.C
    ffgf

---
+++ digits.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    long long n;
+    int count = 0;
+
+    printf("Enter an integer: ");
+    scanf("%lld", &n);
+
+    while(n != 0)
+    {
+        n /= 10;
+        ++count;
+    }
+
+    printf(" the number of digits: %d", count);
+}
