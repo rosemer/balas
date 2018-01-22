13d97458abeba4678b4b4facb06e54957e729d98
rosemer <35221895+rosemer@users.noreply.github.com>
2018-01-22 16:11:13 +0530

    array.C
    array program

---
+++ array.C
@@ -0,0 +1,15 @@
+#include<stcio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c=0,i=0;
+printf("enter a,b");
+scanf("%d%d",&a&b);
+for(i=0;i<=a;i++)
+{
+c=c+i;
+}
+printf("%d,i");
+printf("the sum");
+getch();
+}
